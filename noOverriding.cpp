3053d8cd5a7561013644fb225ccd65be9e8f59d4
Chintya-aa <tyachintyachin@gmail.com>
2025-06-02 15:16:47 +0700

    baseclass final

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -8,16 +8,3 @@ class baseClass {
     }
 };
 
-class serivedclass : public baseClass {
-    public: 
-    void perkenalan() {
-        cout << "Hallo saya Function dari derived class";
-    }
-};
-
-int main() {
-    derivedClass a;
-    a.Perkenalan();
-
-    return 0;
-}
